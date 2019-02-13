dcd0e32714788caca1f824dad26d14a3bbab630c
fksato <f.k.sato@tum.de>
2019-02-13 10:43:06 -0500

    setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='vid_plist',
       version='0.1',
       description='copy move videos from m3u8 playlist to specified folder',
-      url='',
+      url='https://github.com/fksato/vid_plist',
       author='fs',
-      author_email='fksato@mit.edu',
+      author_email='f.k.sato@tum.de',
       license='MIT',
       packages=['vid_plist'],
       zip_safe=False)
